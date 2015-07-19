ac6c53aa5104553de64a71e9f4201ce2e0c3fd93
xsy <xsy@xsydeMacBook-Pro.local>
2015-07-19 11:34:17 +0800

    Initail

---
+++ testgithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testgithub
+//
+//  Created by xsy on 15/7/19.
+//  Copyright (c) 2015年 xsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testgithub
+//
+//  Created by xsy on 15/7/19.
+//  Copyright (c) 2015年 xsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
